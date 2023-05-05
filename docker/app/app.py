07ec26e0d1160d446db382b82aef497d96cb82ac
rtashklzx47277 <rtashklzx47277@gmail.com>
2023-05-05 14:46:10 +0800

    sync db

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from connect_to_db import sql_command
+from functions.connect_to_db import SQLcommand
 from draw_chart import DrawChart
 
 app = Flask(__name__)
@@ -33,24 +33,24 @@ def template5():
 def submit():
   if request.form.get('options'):
     value = request.form.get('options')
-    dataset = sql_command(f'SELECT time, likes, reactions, shares, comments FROM posts WHERE MONTH(time) = {value} AND YEAR(time) = 2023 ORDER BY time')
+    dataset = SQLcommand().get(f'SELECT time, likes, reactions, shares, comments FROM posts WHERE MONTH(time) = {value} AND YEAR(time) = 2023 ORDER BY time')
     data_dict = [{'time': data[0], 'likes': data[1], 'reactions': data[2], 'shares': data[3], 'comments': data[4]} for data in dataset]
     return render_template('index.html', message=f'2023年{value}月', data=data_dict, selected_value=value)
 
   if request.form.get('button') == 'line':
-    dataset = sql_command('SELECT DATE_FORMAT(time, "%Y") AS year, COUNT(*) FROM posts GROUP BY year ORDER BY year')
+    dataset = SQLcommand().get('SELECT DATE_FORMAT(time, "%Y") AS year, COUNT(*) FROM posts GROUP BY year ORDER BY year')
     x, y = [element[0] for element in dataset], [element[1] for element in dataset]
     chart_html = DrawChart().line('Posts of Year', 'Year', 'Posts', x, y)
     return render_template('index.html', chart_html=chart_html)
   
   if request.form.get('button') == 'bar':
-    dataset = sql_command('SELECT DATE_FORMAT(time, "%Y") AS year, COUNT(*) FROM posts GROUP BY year ORDER BY year')
+    dataset = SQLcommand().get('SELECT DATE_FORMAT(time, "%Y") AS year, COUNT(*) FROM posts GROUP BY year ORDER BY year')
     x, y = [element[0] for element in dataset], [element[1] for element in dataset]
     chart_html = DrawChart().bar('Posts of Year', 'Year', 'Posts', x, y)
     return render_template('index.html', chart_html=chart_html)
 
   value = request.form.get('button')
-  dataset = sql_command(f'SELECT time, likes, reactions, shares, comments FROM posts ORDER BY time DESC LIMIT {value}')
+  dataset = SQLcommand().get(f'SELECT time, likes, reactions, shares, comments FROM posts ORDER BY time DESC LIMIT {value}')
   data_dict = [{'time': data[0], 'likes': data[1], 'reactions': data[2], 'shares': data[3], 'comments': data[4]} for data in dataset]
   return render_template('index.html', message=f'資料庫前{value}筆資料', data=data_dict)
 
