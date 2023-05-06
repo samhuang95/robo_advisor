a7d877e6911742ccaa6a52dbbc228b209d65f392
rtashklzx47277 <rtashklzx47277@gmail.com>
2023-05-06 16:14:10 +0800

    dummy

--- docker/app/app.py
+++ docker/app/app.py
@@ -5,32 +5,11 @@ from functions.draw_chart import DrawChart
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html', message='Hello Flask!')
-
-@app.route('/1')
-def template1():
-  return render_template('template1.html')
-
-@app.route('/2')
-def template2():
-  return render_template('template2.html')
-
-@app.route('/3')
-def template3():
-  return render_template('template3.html')
-
-@app.route('/4')
-def template4():
-  return render_template('template4.html')
-
-@app.route('/5')
-def template5():
-  return render_template('template5.html')
-
-@app.route('/', methods=['POST'])
-def submit():
+  if request.method == 'GET':
+    return render_template('index.html', message='Hello Flask!')
+  
   if request.form.get('options'):
     value = request.form.get('options')
     dataset = SQLcommand().get(f'SELECT time, likes, reactions, shares, comments FROM posts WHERE MONTH(time) = {value} AND YEAR(time) = 2023 ORDER BY time')
@@ -53,6 +32,26 @@ def submit():
   dataset = SQLcommand().get(f'SELECT time, likes, reactions, shares, comments FROM posts ORDER BY time DESC LIMIT {value}')
   data_dict = [{'time': data[0], 'likes': data[1], 'reactions': data[2], 'shares': data[3], 'comments': data[4]} for data in dataset]
   return render_template('index.html', message=f'資料庫前{value}筆資料', data=data_dict)
+
+@app.route('/1')
+def template1():
+  return render_template('template1.html')
+
+@app.route('/2')
+def template2():
+  return render_template('template2.html')
+
+@app.route('/3')
+def template3():
+  return render_template('template3.html')
+
+@app.route('/4')
+def template4():
+  return render_template('template4.html')
+
+@app.route('/5')
+def template5():
+  return render_template('template5.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
